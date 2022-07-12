116a97fc4f3260298d8f19f50389209685cb9080
raunak <raunak.singh@getir.com>
2022-07-12 21:06:45 +0530

    Add Repository Implementation

--- lib/data/repository/NoteRepositoryImpl.dart
+++ lib/data/repository/NoteRepositoryImpl.dart
@@ -1,28 +1,28 @@
+import 'package:notes_app_clean_arch_flutter/data/database/DatabaseHelper.dart';
 import 'package:notes_app_clean_arch_flutter/data/model/NoteEntity.dart';
 import 'package:notes_app_clean_arch_flutter/domain/respository/NoteRepository.dart';
 
 class NoteRepositoryImpl implements NoteRepository {
+  DatabaseHelper databaseHelper;
+  NoteRepositoryImpl({required this.databaseHelper});
+
   @override
-  Future<int> deleteNote() {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<int> deleteNote(int id) async {
+    return databaseHelper.deleteNote(id);
   }
 
   @override
-  Future<List<NoteEntity>> getAllNotes() {
-    // TODO: implement getAllNotes
-    throw UnimplementedError();
+  Future<List<NoteEntity>> getAllNotes() async {
+    return await databaseHelper.readAllNotes();
   }
 
   @override
-  Future<int> insertNote(NoteEntity note) {
-    // TODO: implement insertNote
-    throw UnimplementedError();
+  Future<int> insertNote(NoteEntity note) async {
+    return await databaseHelper.insertNote(note);
   }
 
   @override
-  Future<int> updateNote() {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<int> updateNote(NoteEntity note) async {
+    return await databaseHelper.updateNote(note);
   }
 }
--- lib/domain/respository/NoteRepository.dart
+++ lib/domain/respository/NoteRepository.dart
@@ -3,6 +3,6 @@ import 'package:notes_app_clean_arch_flutter/data/model/NoteEntity.dart';
 abstract class NoteRepository {
   Future<int> insertNote(NoteEntity note);
   Future<List<NoteEntity>> getAllNotes();
-  Future<int> updateNote();
-  Future<int> deleteNote();
+  Future<int> updateNote(NoteEntity note);
+  Future<int> deleteNote(int id);
 }
